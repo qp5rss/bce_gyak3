77b9e38964507d33c24dbfdd8f2e01816fbd1b7d
PT <pintertamas98@gmail.com>
2022-10-19 23:04:32 +0200

    MNB service response and result, loading MNB data

--- qp5rss_gyak5/qp5rss_gyak5/Form1.Designer.cs
+++ qp5rss_gyak5/qp5rss_gyak5/Form1.Designer.cs
@@ -28,13 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtbDebug = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtbDebug
+            // 
+            this.txtbDebug.Location = new System.Drawing.Point(13, 13);
+            this.txtbDebug.Name = "txtbDebug";
+            this.txtbDebug.Size = new System.Drawing.Size(255, 425);
+            this.txtbDebug.TabIndex = 0;
+            this.txtbDebug.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtbDebug);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox txtbDebug;
     }
 }
 
--- qp5rss_gyak5/qp5rss_gyak5/Form1.cs
+++ qp5rss_gyak5/qp5rss_gyak5/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using qp5rss_gyak5.MnbServiceReference;
 
 namespace qp5rss_gyak5
 {
@@ -15,6 +16,24 @@ namespace qp5rss_gyak5
         public Form1()
         {
             InitializeComponent();
+            LoadMNB();
+        }
+
+        private void LoadMNB()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+            txtbDebug.Text = result.ToString();
         }
     }
 }
