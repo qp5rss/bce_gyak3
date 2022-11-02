feeb52b80317f88868b547cbf11ea7b26cd13207
PT <pintertamas98@gmail.com>
2022-11-02 20:51:18 +0100

    Added Car class

---
+++ qp5rss_gyak6/qp5rss_gyak6/Entities/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using qp5rss_gyak6.Abstractions;
+
+namespace qp5rss_gyak6.Entities
+{
+    public class Car : Toy
+    {
+        public Car()
+        {
+
+        }
+
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
