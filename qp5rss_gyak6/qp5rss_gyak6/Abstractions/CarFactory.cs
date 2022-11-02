30b522f3870d849956316d8e711fd2fd2da21142
PT <pintertamas98@gmail.com>
2022-11-02 20:55:15 +0100

    Added interface for BallFactory; added the CarFactory class

--- qp5rss_gyak6/qp5rss_gyak6/Abstractions/CarFactory.cs
+++ qp5rss_gyak6/qp5rss_gyak6/Abstractions/CarFactory.cs
@@ -1,17 +1,17 @@
-﻿using qp5rss_gyak6.Abstractions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using qp5rss_gyak6.Entities;
 
-namespace qp5rss_gyak6.Entities
+namespace qp5rss_gyak6.Abstractions
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
