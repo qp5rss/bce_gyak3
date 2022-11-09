826b8196c6ca682093f7ec6cb9c0f139b781fcc8
PT <pintertamas98@gmail.com>
2022-11-09 19:28:03 +0100

    Added method to calculate portfolio value

--- qp5rss_gyak7/qp5rss_gyak7/Form1.cs
+++ qp5rss_gyak7/qp5rss_gyak7/Form1.cs
@@ -34,5 +34,19 @@ namespace qp5rss_gyak7
 
             dataGridViewPortfolio.DataSource = Portfolio;
         }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;  
+            foreach(var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim() && date <= x.TradingDay
+                            select x).First();
+                value += (decimal)last.Price * item.Value;
+            }
+
+            return value;
+        }
     }
 }
