3e30f6265d9674e2ea9f5079673349519affd48b
PT <pintertamas98@gmail.com>
2022-11-27 21:48:12 +0100

    Added base of simulation

--- qp5rss_gyak9/qp5rss_gyak9/Form1.cs
+++ qp5rss_gyak9/qp5rss_gyak9/Form1.cs
@@ -18,6 +18,8 @@ namespace qp5rss_gyak9
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
 
+        Random rng = new Random(777);
+
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +27,22 @@ namespace qp5rss_gyak9
             Population = GetPopulation(@"C:\Temp\nép.csv");
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+
+            for(int year = 2005; year <= 2024; year++)
+            {
+                for(int i = 0; i < Population.Count; i++)
+                {
+
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(string.Format("Év:{0} Férfiak:{1} Nők:{2}", year, nbrOfMales, nbrOfFemales))
+            }
         }
 
         public List<Person> GetPopulation(string csvpath)
